a1361691521445e4130e7b6c949e55e53aa06647
Nathan Dobrowolski <Nathan.Dobrowolski@nordstrom.com>
2015-04-15 10:42:23 -0700

    lib modules working.

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-var get = require('./lib/get');
+var get  = require('./lib/get'),
+    API  = require('./lib/models/api')
+    Item = require('./lib/models/item');
 
 get('http://localhost/api').then(function (api) {
     console.log('GET http://localhost/api');
@@ -23,4 +25,29 @@ get('http://localhost/api/items/2').then(function (api) {
 get('http://localhost/api/items/3').then(function (api) {
     console.log('GET http://localhost/api/items/3');
     console.log(api);
+});
+
+API.retrieve().then(function (api) {
+    console.log('API.retrieve()');
+    console.log(api);
+});
+
+Item.search().then(function (results) {
+    console.log('Item.search()');
+    console.log(results);
+});
+
+Item.retrieve(1).then(function (results) {
+    console.log('Item.retrieve(1)');
+    console.log(results);
+});
+
+Item.retrieve(2).then(function (results) {
+    console.log('Item.retrieve(2)');
+    console.log(results);
+});
+
+Item.retrieve(3).then(function (results) {
+    console.log('Item.retrieve(3)');
+    console.log(results);
 });
